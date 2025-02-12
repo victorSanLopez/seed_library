02631d2635947cc8d60049202c847061a4193cdc
Sanesthesie <victorsanlopez@gmail.com>
2025-02-12 20:16:32 +0100

    feat: create navbar for seed library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
