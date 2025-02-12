31bee7f5279a2cd40deabaaf16cb79d55954984e
Sanesthesie <victorsanlopez@gmail.com>
2025-02-12 17:19:40 +0100

    fix: caps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
 import { ToastContainer } from "react-toastify";
 import "./App.css";
-import SignInPopup from "./components/signInPopup/SignInPopUp";
-import UserRegistrationForm from "./components/userRegistrationForm/UserRegistrationForm";
 
 function App() {
   return (
     <>
-      <UserRegistrationForm />
-      <SignInPopup />
       <ToastContainer />
     </>
   );
