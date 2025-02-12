b97514615cbc5fce76a77a0c9b4a5602018878dc
Sanesthesie <victorsanlopez@gmail.com>
2025-02-12 17:17:48 +0100

    feat: Create signin popup / Fix middlewares

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { ToastContainer } from "react-toastify";
 import "./App.css";
+import SignInPopup from "./components/signInPopup/SignInPopUp";
 import UserRegistrationForm from "./components/userRegistrationForm/UserRegistrationForm";
 
 function App() {
   return (
     <>
       <UserRegistrationForm />
+      <SignInPopup />
       <ToastContainer />
     </>
   );
--- server/src/middlewares/validatePassword.middleware.ts
+++ server/src/middlewares/validatePassword.middleware.ts
@@ -3,14 +3,14 @@ import { comparePasswords } from "../helpers/passwordUtils";
 
 export const validatePassword: RequestHandler = async (req, res, next) => {
   try {
-    const { hashedPassword, inputPassword } = req.body;
+    const { hashedPassword, password } = req.body;
 
-    if (!hashedPassword || !inputPassword) {
+    if (!hashedPassword || !password) {
       res.status(400).json({ message: "Missing password fields" });
       return;
     }
 
-    const isValid = await comparePasswords(hashedPassword, inputPassword);
+    const isValid = await comparePasswords(hashedPassword, password);
 
     if (!isValid) {
       res.status(403).json({ message: "Invalid credentials" });
