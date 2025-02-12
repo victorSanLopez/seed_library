afe8ce6da6c20ab14b4627c76b46552872ac5bff
Sanesthesie <victorsanlopez@gmail.com>
2025-02-12 15:42:29 +0100

    fix: remove unused features

--- server/src/helpers/tokenUtils.ts
+++ server/src/helpers/tokenUtils.ts
@@ -6,13 +6,3 @@ export const generateToken = async (payload: PayloadType): Promise<string> => {
     expiresIn: "24h",
   });
 };
-
-export const decodeToken = (token: string) => {
-  return jwt.decode(token);
-};
-
-// export const isTokenExpired = (token: string): boolean => {
-//   const decoded = jwt.decode(token) as { exp?: number } | null;
-//   if (!decoded || !decoded.exp) return true;
-//   return decoded.exp * 1000 < Date.now();
-// };
