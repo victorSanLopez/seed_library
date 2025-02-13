d773db0aaedb79e9996739b8917f7f62a06a6948
Sanesthesie <victorsanlopez@gmail.com>
2025-02-13 16:53:28 +0100

    feat: Create a link to the landing page in the footer close#2

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
 import style from "./footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={style.footerContainer}>
-      <img
-        className={style.footerLogo}
-        src="/images/logo_v1.png"
-        alt="Logo du site représentant trois feuilles"
-      />
+      <Link to="/">
+        <img
+          className={style.footerLogo}
+          src="/images/logo_v1.png"
+          alt="Logo du site représentant trois feuilles"
+        />
+      </Link>
       <p className={style.text}>
         Ce site a été développé dans un temps limité dans le cadre de ma
         formation à la Wild Code School. Il a pour but de faciliter
